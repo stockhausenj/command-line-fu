244e3b8b08d3c6c48dcde5ac7defa4e069c65d22
Jay Stockhausen <jay.stockhausen@gmail.com>
2017-03-13 20:10:21 -0500

    Added waxing info to output

--- phase-of-moon.sh
+++ phase-of-moon.sh
@@ -1,20 +1,36 @@
 #!/bin/bash
 
 url="http://www.moongiant.com/phase/today"
+
 pattern="Illumination"
+
 phase="$( curl -s "$url" | grep "$pattern" | tr ',' "\n" | grep "$pattern" | sed 's/[^0-9]//g' )"
 
-if [ $phase -lt 5 ] ; then
-  phasename="new"
-elif [ $phase -lt 45 ] ; then
-  phasename="crescent"
-elif [ $phase -lt 55 ] ; then
-  phasename="quarter"
-elif [ $phase -lt 95 ] ; then
-  phasename="gibbous"
+url_ago="http://www.moongiant.com/phase"
+
+eval $( date --date="1 days ago" +"mon=%m date=%d year=%Y" )
+
+ydayurl="$url_ago/$mon/$day/$year"
+
+yillumlevel="$( curl -s "$ydayurl" | grep "$pattern" | tr ',' "\n" | grep "$pattern" | sed 's/[^0-9]//g' )"
+
+
+if [ $phase -gt $yillumlevel ]; then
+  waxwane="waxing"
 else
-  phasename="full"
+  waxwane="waning"
 fi
 
-echo "The moon is currently $phasename with $phase illuminated"
+if [ $phase -lt 5 ] ; then                                                                           
+  phasename="new"                                                                                    
+elif [ $phase -lt 45 ] ; then                                                                        
+  phasename="$waxwane crescent"
+elif [ $phase -lt 55 ] ; then                                                                        
+  phasename="quarter"                                                       
+elif [ $phase -lt 95 ] ; then                                                                        
+  phasename="$waxwane gibbous"
+else
+  phasename="full"
+fi 
 
+echo "The moon is currently $phasename with $phase illuminated"
